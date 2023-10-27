f01ae45b5ed8cdc8d904cd7b85537f1ed03d1170
MurilloDiana <dianamurillo.467@gmail.com>
2023-10-27 11:20:37 -0400

    inicio

---
+++ src/views/admin/dataTables/variables/columnsData.js
@@ -0,0 +1,75 @@
+export const columnsDataDevelopment = [
+  {
+    Header: "NOMBRE",
+    accessor: "name",
+  },
+  {
+    Header: "TECH",
+    accessor: "tech",
+  },
+  {
+    Header: "DATE",
+    accessor: "date",
+  },
+  {
+    Header: "PROGRESS",
+    accessor: "progress",
+  },
+];
+
+export const columnsDataCheck = [
+  {
+    Header: "NOMBRE",
+    accessor: "name",
+  },
+  {
+    Header: "PROGRESO",
+    accessor: "progress",
+  },
+  {
+    Header: "CANTIDAD",
+    accessor: "quantity",
+  },
+  {
+    Header: "FECHA",
+    accessor: "date",
+  },
+];
+
+export const columnsDataColumns = [
+  {
+    Header: "NOMBRE",
+    accessor: "name",
+  },
+  {
+    Header: "PROGRESO",
+    accessor: "progress",
+  },
+  {
+    Header: "CANTIDAD",
+    accessor: "quantity",
+  },
+  {
+    Header: "FECHA",
+    accessor: "date",
+  },
+];
+
+export const columnsDataComplex = [
+  {
+    Header: "NOMBRE",
+    accessor: "name",
+  },
+  {
+    Header: "ESTATUS",
+    accessor: "status",
+  },
+  {
+    Header: "FECHA",
+    accessor: "fecha",
+  },
+  {
+    Header: "PROGRESO",
+    accessor: "PROGRESO",
+  },
+];
